e7d76f5de81e6e2f6d49d35c5e390b445b1eb3e6
Antonio3veces3 <cramirez28@ucol.mx>
2023-06-18 23:52:18 -0600

    Implement Jest to apply testing creating components

--- InmobiliariaApp/__tests__/App-test.tsx
+++ InmobiliariaApp/__tests__/App-test.tsx
@@ -4,11 +4,34 @@
 
 import 'react-native';
 import React from 'react';
+import {estatesList} from '../data/data.json'
 import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import HousesList from '../components/HousesList';
+import HouseItem, { House } from '../components/HouseItem';
+import ImageHouse from '../components/ImageHouse';
+import InfoHouse from '../components/InfoHouse';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+const data: House[]= estatesList;
+
+describe("Test create House components",()=>{
+    it('Create Houses List', () => {
+      renderer.create(<HousesList/>);
+    });
+
+    it('Create House Item', () => {
+        const house= data[0]
+      renderer.create(<HouseItem house={house}/>);
+    });
+
+    it('Create Image House component',()=>{
+        const {img, rating}= data[0];
+        renderer.create(<ImageHouse img={img} rating={rating} />)
+    });
+
+    it('Create Info House component',()=>{
+        const house = data[0];
+        renderer.create(<InfoHouse house={house} />)
+    })
+})
